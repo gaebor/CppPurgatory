ae0b390724cbbfd2c6d96489a893d6621948dddf
gaebor <gaebor@gmail.com>
2016-04-13 09:54:43 +0200

    explanatory comment fix

--- src/const_list.cpp
+++ src/const_list.cpp
@@ -6,9 +6,9 @@
 /************************************************************************/
 /* In Visual Studio 2013 it works                                       */
 /* In g++ , it doesn't                                                  */
-/* As @ngg pointed out, this is forbidden per standard.                 */
+/* As https://github.com/ngg pointed out, this is forbidden per standard*/
 /* I don't know why Visual Studio allowed it,                           */
-/* but in Visual Studio 2013 it is forbidden                            */
+/* but in Visual Studio 2015 it is forbidden                            */
 /************************************************************************/
 
 int main(int argc, char* argv[])
