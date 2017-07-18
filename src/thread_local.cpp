17be9ae72cca268fe1e2370b763b5dec87b6e54b
gaebor <gaebor@gmail.com>
2017-07-18 13:49:39 +0200

    thread local

--- src/thread_local.cpp
+++ src/thread_local.cpp
@@ -1,3 +1,13 @@
+/*********************************************
+This program starts several threads
+and initializes a thread-local variable in all of them.
+
+C++11 standard permits this.
+in VS2013 you get
+    error C2483: object with constructor or destructor cannot be declared 'thread'
+in VS2017 it works fine!
+*********************************************/
+
 #include <vector>
 #include <iostream>
 #include <thread>
@@ -14,17 +24,20 @@ struct B
     ~B(){ std::cout << "Destructor!" << std::endl; }
 };
 
-thread_local B b; //error C2483: object with constructor or destructor cannot be declared 'thread'
+thread_local B b;
 
 int main(int argc, char* argv[])
 {
     std::vector<std::shared_ptr<std::thread>> threads;
+    b.i += 1;
     for (int i = 0; i < argc; ++i)
     {
         threads.emplace_back(new std::thread([](){std::cout << b.i << std::endl;}));
     }
     for (int i = 0; i < argc; ++i)
         threads[i]->join();
-
+    
+    std::cout << b.i << std::endl;
+    
     return 0;
 }
